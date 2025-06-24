e3d350d71fdfdc3cae9ac89d70b1d1748792367d
Dipesh77Dev <rajpersonal777@gmail.com>
2025-06-25 01:11:52 +0530

    normal-intro 25-6-25

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+import Sarin from './components/sarin'
+
+function App () {
+  return (
+    <div>
+      <p>sairin</p>
+      <p>Ashish</p>
+      <p>Dipesh</p>
+      {/* <Sarin></Sarin> */}
+      <Sarin />
+    </div>
+  )
+}
+
+export default App
